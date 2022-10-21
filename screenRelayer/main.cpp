773c26cb91905823717e7e5330c627689853afa5
Braised-Lamb <lambsjtu@outlook.com>
2022-10-21 22:13:50 +0800

    added icons

--- screenRelayer/main.cpp
+++ screenRelayer/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     screenRelayer w;
+    w.setWindowIcon(QIcon(":/screenRelayer/icons/logo.png"));
     w.show();
     return a.exec();
 }
