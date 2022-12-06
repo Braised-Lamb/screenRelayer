89f7f12478077071873aa13f9bee88233234612b
Braised-Lamb <lambsjtu@outlook.com>
2022-12-06 19:44:39 +0800

    start the window in second screen

--- screenRelayer/main.cpp
+++ screenRelayer/main.cpp
@@ -3,9 +3,12 @@
 
 int main(int argc, char *argv[])
 {
+    QDesktopWidget* desktop = QApplication::desktop();
+    int N = desktop->screenCount();
     QApplication a(argc, argv);
     screenRelayer w;
     w.setWindowIcon(QIcon(":/screenRelayer/icons/logo.png"));
+    if (N >= 1) w.setGeometry(desktop->screenGeometry(1));
     w.show();
     return a.exec();
 }
