da4d1cc28f244c2b7983aa0eefec23d85b5bd2fe
Braised-Lamb <lambsjtu@outlook.com>
2022-12-06 20:43:43 +0800

    show window in the last screen

--- screenRelayer/main.cpp
+++ screenRelayer/main.cpp
@@ -3,8 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication a(argc, argv); 
+    QDesktopWidget* desktop = QApplication::desktop();
+    auto N = desktop->screenCount();
+    qDebug() << N;
     screenRelayer w;
+    w.setGeometry(desktop->screenGeometry(N - 1));
     w.setWindowIcon(QIcon(":/screenRelayer/icons/logo.png"));
     w.show();
     return a.exec();
